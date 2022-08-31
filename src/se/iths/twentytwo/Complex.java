cfddd0b5319747e1c276d9244b49054e0dfe8d94
Martin Blomberg <martin.blomberg@outlook.com>
2022-08-31 13:52:05 +0200

    Add Complex example

---
+++ src/se/iths/twentytwo/Complex.java
@@ -0,0 +1,34 @@
+package se.iths.twentytwo;
+
+public class Complex {
+    public static void main(String[] args) {
+
+        int i = 0;
+
+        Box box = new Box();
+        box.width = 10;
+        System.out.println(box.width);
+
+        Box largeBox = new Box();
+        largeBox.height = 100;
+        largeBox.length = 20;
+        largeBox.width = 30;
+
+        System.out.println(box.width);
+
+        System.out.println(largeBox.height);
+        System.out.println(largeBox.length);
+        System.out.println(largeBox.width);
+
+        String string = "This is a String";
+        String string2 = "";
+        String string3 = new String();
+
+    }
+}
+
+class Box {
+    int length;
+    int width;
+    int height;
+}
