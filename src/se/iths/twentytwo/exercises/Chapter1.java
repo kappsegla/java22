c84a6944a84a3375a9ed18c782502ed76c83d902
Martin Blomberg <martin.blomberg@outlook.com>
2022-09-01 19:51:05 +0200

    Add exercises package
    Chapter1.java and Chapter2.java templates

---
+++ src/se/iths/twentytwo/exercises/Chapter1.java
@@ -0,0 +1,31 @@
+package se.iths.twentytwo.exercises;
+
+public class Chapter1 {
+    public static void main(String[] args) {
+        task2();
+    }
+
+    public static void task2() {
+
+    }
+
+    public static void task3() {
+
+    }
+
+    public static void task4() {
+
+    }
+
+    public static void task5() {
+
+    }
+
+    public static void task6() {
+
+    }
+
+    public static void task7() {
+
+    }
+}
---
+++ src/se/iths/twentytwo/exercises/Chapter2.java
@@ -0,0 +1,44 @@
+package se.iths.twentytwo.exercises;
+
+public class Chapter2 {
+
+    public static void main(String[] args) {
+        task3();
+    }
+
+    public static void task3() {
+
+    }
+
+    public static void task4() {
+
+    }
+
+    public static void task5() {
+
+    }
+
+    public static void task6() {
+
+    }
+
+    public static void task7() {
+
+    }
+
+    public static void task8() {
+
+    }
+
+    public static void task9() {
+
+    }
+
+    public static void task10() {
+
+    }
+
+    public static void task11() {
+
+    }
+}
