b5945d9cfa3ff89cf6ce348cacc6c58bfafa0af3
Martin Blomberg <martin.blomberg@outlook.com>
2022-09-02 10:51:30 +0200

    Add code for exercises chapter1

--- src/se/iths/twentytwo/exercises/Chapter2.java
+++ src/se/iths/twentytwo/exercises/Chapter2.java
@@ -1,5 +1,7 @@
 package se.iths.twentytwo.exercises;
 
+import java.math.BigDecimal;
+
 public class Chapter2 {
 
     public static void main(String[] args) {
@@ -7,7 +9,17 @@ public class Chapter2 {
     }
 
     public static void task3() {
+        String password = "1234";
+        System.out.println("Skriv in ditt lösenord:");
+        char[] enteredPassword = System.console().readPassword();
 
+        String enteredPasswordString = String.valueOf(enteredPassword);
+        if (enteredPasswordString.equals(password)) {
+            System.out.println("Rätt lösenord!");
+        } else {
+            System.out.println("Fel lösenord!");
+        }
+        ///String passwordString = new String(inputPassword); // Gör om lösenord till string
     }
 
     public static void task4() {
@@ -27,7 +39,8 @@ public class Chapter2 {
     }
 
     public static void task8() {
-
+        //pax
+        // Julia paxaren
     }
 
     public static void task9() {
