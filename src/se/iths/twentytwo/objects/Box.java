7ffb548bbd22075edf08ed5293f70b70aee5bd04
Martin Blomberg <martin.blomberg@outlook.com>
2022-09-07 10:49:48 +0200

    Add Box functionality

--- src/se/iths/twentytwo/objects/Box.java
+++ src/se/iths/twentytwo/objects/Box.java
@@ -1,5 +1,21 @@
 package se.iths.twentytwo.objects;
 
 public class Box {
+    int height;
+    int width;
+    int length;
+
+    public void increaseBoxSize() {
+        height = height * 2;
+        width = width * 2;
+        length = length * 2;
+    }
+
+    public void printBoxInfo() {
+        System.out.println("Width: " + width);
+        System.out.println("Height: " + height);
+        System.out.println("Length: " + length);
+    }
+
 
 }
--- src/se/iths/twentytwo/objects/BoxDemo.java
+++ src/se/iths/twentytwo/objects/BoxDemo.java
@@ -2,4 +2,24 @@ package se.iths.twentytwo.objects;
 
 public class BoxDemo {
 
+    public static void main(String[] args) {
+        int i = 2;
+
+        Box smallBox = new Box();
+        smallBox.height = 10;
+        smallBox.width = 5;
+        smallBox.length = 10;
+        smallBox.printBoxInfo();
+        //increaseBoxSize(smallBox);
+        smallBox.increaseBoxSize();
+
+        smallBox.printBoxInfo();
+    }
+
+    public static void increaseBoxSize(Box box) {
+        //Multiply all dimensions by 2
+        box.length = box.length * 2;
+        box.width = box.width * 2;
+        box.height = box.height * 2;
+    }
 }
