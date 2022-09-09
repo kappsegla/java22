e584499b01ffaaa5618b1fa37c311f683276f767
Martin Blomberg <martin.blomberg@outlook.com>
2022-09-09 09:20:33 +0200

    Chapter3

--- src/se/iths/twentytwo/exercises/Chapter3.java
+++ src/se/iths/twentytwo/exercises/Chapter3.java
@@ -1,12 +1,29 @@
 package se.iths.twentytwo.exercises;
 
+import java.util.Scanner;
+
 public class Chapter3 {
+
+    //public static Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
-        task1();
+        Scanner sc = new Scanner(System.in);
+        task1(sc);
     }
 
-    private static void task1() {
+    private static double task1(Scanner sc) {
+        while (true) {
+            try {
+                String str = sc.nextLine();
+                double aDouble = Double.parseDouble(str);
+                System.out.println(aDouble);
+                return aDouble;
+            } catch (Exception e) {
+                System.out.println("Error");
+            }
 
+        }
 
     }
+
 }
