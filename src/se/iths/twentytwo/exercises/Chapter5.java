afa0ef3cf2690b8e8093953a7adacaa793b45932
Martin Blomberg <martin.blomberg@outlook.com>
2022-09-09 13:58:16 +0200

    Chapter5

--- src/se/iths/twentytwo/exercises/Chapter5.java
+++ src/se/iths/twentytwo/exercises/Chapter5.java
@@ -1,7 +1,26 @@
 package se.iths.twentytwo.exercises;
 
+import java.util.Arrays;
+
 public class Chapter5 {
     public static void main(String[] args) {
 
+        double d = 5.5;
+        f5(d);
+        System.out.println(d);
+
+        double[] dArr = {5.5};
+        f5(Arrays.copyOf(dArr, dArr.length));
+        System.out.println(dArr[0]);
+
     }
+
+    public static void f5(double d) {
+        d++;
+    }
+
+    public static void f5(double[] dArr) {
+        dArr[0]++;
+    }
+
 }
