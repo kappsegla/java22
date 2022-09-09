f4b02380337554438dee7692eb209a3ceb5ded29
Martin Blomberg <martin.blomberg@outlook.com>
2022-09-09 10:26:18 +0200

    Chapter4

--- src/se/iths/twentytwo/exercises/Chapter4.java
+++ src/se/iths/twentytwo/exercises/Chapter4.java
@@ -1,10 +1,16 @@
 package se.iths.twentytwo.exercises;
 
 import java.util.Arrays;
+import java.util.Random;
+
 
 public class Chapter4 {
+
+    public static Random rand = new Random();
+
+
     public static void main(String[] args) {
-        task2();
+        task3();
     }
 
     private static void task1() {
@@ -26,5 +32,29 @@ public class Chapter4 {
                 sum -= arr[i];
         }
         System.out.println("sum = " + sum);
+    }
+
+    public static void task3() {
+
+        int[] arr = new int[5];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = rand.nextInt(1, 21);
+        }
+
+        int oddCounter = 0;
+        int min = arr[0];
+        int max = arr[0];
+        for (int j : arr) {
+            if (j < min)
+                min = j;
+            if (j > max)
+                max = j;
+            if (j % 2 == 1)
+                oddCounter++;
+        }
+        System.out.println(Arrays.toString(arr));
+        System.out.println("Min: " + min);
+        System.out.println("Max: " + max);
+        System.out.println("Antal udda tal:" + oddCounter);
     }
 }
