36054a72b7ce873bf940b542d19613b13b9d577e
Martin Blomberg <martin.blomberg@outlook.com>
2022-09-09 09:48:47 +0200

    Chapter4

--- src/se/iths/twentytwo/exercises/Chapter4.java
+++ src/se/iths/twentytwo/exercises/Chapter4.java
@@ -1,11 +1,30 @@
 package se.iths.twentytwo.exercises;
 
+import java.util.Arrays;
+
 public class Chapter4 {
     public static void main(String[] args) {
-        task1();
+        task2();
     }
 
     private static void task1() {
+        String[] colors = {"Gul", "Grön", "Orange", "Blå", "Grå"};
+        for (int i = 0; i < colors.length; i++) {
+            System.out.println(colors[i]);
+        }
+        System.out.println(Arrays.toString(colors));
+    }
 
+
+    private static void task2() {
+        int[] arr = {2, 3, 5, 8, 13, 21, 34};
+        int sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (i % 2 == 0)
+                sum += arr[i];
+            else
+                sum -= arr[i];
+        }
+        System.out.println("sum = " + sum);
     }
 }
