61e54fa321cae408ad68c28214e59075ba70350b
Martin Blomberg <martin.blomberg@outlook.com>
2022-09-13 14:47:02 +0200

    Add Circle

---
+++ src/se/iths/twentytwo/shapes/Circle.java
@@ -0,0 +1,64 @@
+package se.iths.twentytwo.shapes;
+
+import java.util.Arrays;
+
+public class Circle {
+    int radius;
+
+    public Circle(int radius) {
+        this.radius = radius;
+    }
+
+    public Circle(Circle circle){
+        this.radius = circle.radius;
+    }
+
+    public Circle copyOf(){
+        return new Circle(this);
+    }
+
+    public int getRadius() {
+        return radius;
+    }
+
+    public static void main(String[] args) {
+        Circle c = new Circle(42);
+        Circle refc = c;
+        Circle copyOfC = new Circle(c);
+        Circle secondCopy = c.copyOf();
+
+        c.radius++;
+        System.out.println("c.radius = " + c.getRadius());
+        System.out.println("refc.radius = " + refc.getRadius());
+        System.out.println("copyOfC.radius = " + copyOfC.getRadius());
+
+        int[] array = {1,2,3};
+
+        int[] copyOfArray = new int[array.length];
+
+        for (int i = 0; i < array.length; i++) {
+            copyOfArray[i] = array[i];
+        }
+
+        array[0] = 7;
+        System.out.println(Arrays.toString(array));
+        System.out.println(Arrays.toString(copyOfArray));
+
+        Circle[] circles = {new Circle(42), new Circle(43)};
+
+        Circle[] copyOfCircles = new Circle[circles.length];
+
+        for (int i = 0; i < circles.length; i++) {
+            //Deep copy, new objects are created
+            copyOfCircles[i] = circles[i].copyOf();
+            //Shallow copy, both arrays points to same Circle object
+            //copyOfCircles[i] = circles[i];
+        }
+
+        circles[0].radius = 77;
+        System.out.println(circles[0].radius);
+        System.out.println(copyOfCircles[0].radius);
+
+
+    }
+}
