1f0bb213db055c6a83ffacdf49c2a95f79dac4a2
Martin Blomberg <martin.blomberg@outlook.com>
2022-09-14 11:29:37 +0200

    Add Cylinder

--- src/se/iths/twentytwo/shapes/Cylinder.java
+++ src/se/iths/twentytwo/shapes/Cylinder.java
@@ -1,11 +1,11 @@
 package se.iths.twentytwo.shapes;
 
-public class Cylinder {
-    private final Circle circle;
+public class Cylinder extends Circle {
+
     private final int height;
 
     public Cylinder(int radius, int height) {
-        this.circle = new Circle(radius);
+        super(radius);
         this.height = height;
     }
 
@@ -13,12 +13,62 @@ public class Cylinder {
         return height;
     }
 
-    public int getRadius() {
-        return circle.getRadius();
+    public double getVolume() {
+        return getArea() * height;
     }
 
-    public double getVolume() {
-        return circle.getArea() * height;
+    public static void main(String[] args) {
+        Cylinder cylinder = new Cylinder(2, 3);
+
+        Circle circle = new Cylinder(1, 2);
+
+        Cylinder cylinder1 = (Cylinder) circle;
+
+        Object object = cylinder1;
+
+        if (object.getClass() == Cylinder.class) {
+            Cylinder cylinder2 = (Cylinder) object;
+            System.out.println(cylinder2.getVolume());
+        }
+        if (object instanceof Cylinder) {
+            Cylinder cylinder2 = (Cylinder) object;
+            System.out.println(cylinder2.getVolume());
+        }
+        if (object instanceof Cylinder cylinder2) {
+            System.out.println(cylinder2.getVolume());
+        }
+        myMethod(object);
+
     }
+
+    public static void  myMethod(Object object){
+        if (object instanceof Cylinder cylinder2) {
+            System.out.println(cylinder2.getVolume());
+        }
+    }
+
 
 }
+
+
+//public class Cylinder {
+//    private final Circle circle;
+//    private final int height;
+//
+//    public Cylinder(int radius, int height) {
+//        this.circle = new Circle(radius);
+//        this.height = height;
+//    }
+//
+//    public int getHeight() {
+//        return height;
+//    }
+//
+//    public int getRadius() {
+//        return circle.getRadius();
+//    }
+//
+//    public double getVolume() {
+//        return circle.getArea() * height;
+//    }
+//}
