38ea3486e7392f8e02c3bfb44a1f127870508cef
Martin Blomberg <martin.blomberg@outlook.com>
2022-09-16 09:22:07 +0200

    Add printAnimal

---
+++ src/se/iths/twentytwo/exercises/chapter6/Animal.java
@@ -0,0 +1,14 @@
+package se.iths.twentytwo.exercises.chapter6;
+
+public class Animal {
+    public int weight;
+    public int height;
+    public String sound;
+
+    public void printAnimal() {
+        System.out.println("animal's weight is " + weight);
+        System.out.println("animal's height is " + height);
+        System.out.println("animal's sound is " + sound);
+    }
+
+}
