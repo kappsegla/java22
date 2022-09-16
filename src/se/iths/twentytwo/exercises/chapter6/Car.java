a366596df40c46bde66e6cce97bf8f88f49e28b7
Martin Blomberg <martin.blomberg@outlook.com>
2022-09-16 10:37:04 +0200

    Add Car constructors

--- src/se/iths/twentytwo/exercises/chapter6/Car.java
+++ src/se/iths/twentytwo/exercises/chapter6/Car.java
@@ -44,4 +44,21 @@ public class Car {
         this.price = price;
         this.model = model;
     }
+
+//    public int halfPrice() {
+//        return price/2;
+//    }
+
+    //This is a command method. Preferred return type is void from a method that updates our fields.
+    //Query methods returning field values should not update them.
+    //This is called Command-Query separation
+    public void halfPrice() {
+        price = price / 2;
+    }
+
+    public static void main(String[] args) {
+        Car car = new Car("Model 3", 800000, Color.RED);
+        car.halfPrice();
+        System.out.println(car.getPrice());
+    }
 }
