7f14309cff1ef36fbb9cf4868f76d8594cb74ffa
Martin Blomberg <martin.blomberg@outlook.com>
2022-09-16 09:35:13 +0200

    Add animals

---
+++ src/se/iths/twentytwo/exercises/chapter6/PlayWithAnimals.java
@@ -0,0 +1,13 @@
+package se.iths.twentytwo.exercises.chapter6;
+
+public class PlayWithAnimals {
+    public static void main(String[] args) {
+        Animal dog = new Animal(50, 40, "Woof!");
+        Animal cat = new Animal(28, 30, "Meow!");
+        Animal ostrich = new Animal(40, 140, "Bom!"); //dem gör bom heter det /Julia :)
+
+        dog.printAnimal();
+
+    }
+
+}
