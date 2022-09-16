63e8a78d2b276223edb43b5d5088ba43045a1f68
Martin Blomberg <martin.blomberg@outlook.com>
2022-09-16 11:13:51 +0200

    Add Counter

--- src/se/iths/twentytwo/exercises/chapter6/Counter.java
+++ src/se/iths/twentytwo/exercises/chapter6/Counter.java
@@ -1,7 +1,16 @@
 package se.iths.twentytwo.exercises.chapter6;
 
+import java.io.IOException;
+
 public class Counter {
     private int number;
+    private static int nInstances;
+
+    public Counter() {
+        number = 0;
+        nInstances++;
+    }
+
 
     public void increase() {
         number++;
@@ -15,11 +24,24 @@ public class Counter {
         return number;
     }
 
+    public void print() {
+        System.out.println("Your counter is at: " + number);
+        System.out.println("Number of counters in your system: " + nInstances);
+    }
+
     public static void main(String[] args) {
         Counter count = new Counter();
+        Counter counter2 = new Counter();
         count.increase();
         System.out.println(count.value());
         count.decrease();
         System.out.println(count.value());
+        counter2.print();
+
+        try {
+            System.in.read();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
