97ca68f136c6b40c2d516ffe0df270a2b5d0bec1
Martin Blomberg <martin.blomberg@outlook.com>
2022-09-16 10:02:08 +0200

    Add Car

---
+++ src/se/iths/twentytwo/exercises/chapter6/Car.java
@@ -0,0 +1,35 @@
+package se.iths.twentytwo.exercises.chapter6;
+
+import java.awt.*;
+
+public class Car {
+    private String model;
+    private int price;
+    private Color colour;
+
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public Color getColour() {
+        return colour;
+    }
+
+    public void setColour(Color colour) {
+        this.colour = colour;
+    }
+
+}
