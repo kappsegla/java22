740f9ea9ade556eb89a2457a812095b1fcc62862
Martin Blomberg <martin.blomberg@outlook.com>
2022-09-21 09:32:10 +0200

    Add Person

--- src/se/iths/twentytwo/records/Person.java
+++ src/se/iths/twentytwo/records/Person.java
@@ -1,5 +1,53 @@
 package se.iths.twentytwo.records;
 
-public class Person {
+import java.util.Objects;
 
+//Immutable - can not be changed after initialization
+public class Person {
+    private final String firstName;
+    private final String lastName;
+    private final int age;
+
+    public Person(String firstName, String lastName, int age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getName(){
+        return firstName + " " + lastName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", age=" + age +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Person person)) return false;
+        return age == person.age && Objects.equals(firstName, person.firstName) && Objects.equals(lastName, person.lastName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(firstName, lastName, age);
+    }
 }
--- src/se/iths/twentytwo/records/RecordsDemo.java
+++ src/se/iths/twentytwo/records/RecordsDemo.java
@@ -2,4 +2,9 @@ package se.iths.twentytwo.records;
 
 public class RecordsDemo {
 
+    public static void main(String[] args) {
+        Person donald = new Person("Donald","Duck",88);
+
+        System.out.println(donald);
+    }
 }
