182d947c9c6df6887da0b8a5bc227bcc141027d8
Martin Blomberg <martin.blomberg@outlook.com>
2022-09-23 10:07:42 +0200

    Add call to super

--- src/se/iths/twentytwo/exercises/chapter6/inheritance/Citrus.java
+++ src/se/iths/twentytwo/exercises/chapter6/inheritance/Citrus.java
@@ -3,6 +3,7 @@ package se.iths.twentytwo.exercises.chapter6.inheritance;
 public class Citrus extends Fruit {
     @Override
     public void printFruit() {
+        super.printFruit();
         System.out.println("This is a citrusfruit");
     }
 
