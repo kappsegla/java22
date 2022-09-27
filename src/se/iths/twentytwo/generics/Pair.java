a1205258242921fafe0dc56f142fc70d5b5aec9c
Martin Blomberg <martin.blomberg@outlook.com>
2022-09-27 11:04:02 +0200

    Add Pair implementation

--- src/se/iths/twentytwo/generics/Pair.java
+++ src/se/iths/twentytwo/generics/Pair.java
@@ -1,4 +1,40 @@
 package se.iths.twentytwo.generics;
 
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.Objects;
+import java.util.Set;
+
 public class Pair {
+    public final String first;
+    public final String second;
+
+    private Pair(String first, String second) {
+        this.first = first;
+        this.second = second;
+    }
+
+    public static Pair of(String first, String second) {
+        return new Pair(first, second);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Pair pair)) return false;
+        return Objects.equals(first, pair.first) && Objects.equals(second, pair.second);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(first, second);
+    }
+
+    @Override
+    public String toString() {
+        return "Pair{" +
+                "first='" + first + '\'' +
+                ", second='" + second + '\'' +
+                '}';
+    }
 }
