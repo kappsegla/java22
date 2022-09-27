e17a7f2eaa9fce41462f01bd87547b9640428e88
Martin Blomberg <martin.blomberg@outlook.com>
2022-09-27 11:43:02 +0200

    Add record Demo

--- src/se/iths/twentytwo/generics/Demo.java
+++ src/se/iths/twentytwo/generics/Demo.java
@@ -1,5 +1,7 @@
 package se.iths.twentytwo.generics;
 
+import java.util.Arrays;
+import java.util.Objects;
 import java.util.Scanner;
 
 public class Demo {
@@ -7,9 +9,14 @@ public class Demo {
         Pair twoStrings = readTwoStrings();
 
         System.out.println(twoStrings);
+
+        String[] twoStringsInArray = readTwoStringsAsArray();
+        System.out.println(Arrays.toString(twoStringsInArray));
+
+        System.out.println(readTwoStringsAsRecord());
     }
 
-    public static Pair readTwoStrings(){
+    public static Pair readTwoStrings() {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter a string: ");
         String stringOne = scanner.nextLine();
@@ -19,4 +26,26 @@ public class Demo {
         return Pair.of(stringOne, stringTwo);
     }
 
+    public static String[] readTwoStringsAsArray() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String stringOne = scanner.nextLine();
+        System.out.print("Enter a second string: ");
+        String stringTwo = scanner.nextLine();
+
+        return new String[]{stringOne, stringTwo};
+    }
+
+    public static TwoValues readTwoStringsAsRecord() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String stringOne = scanner.nextLine();
+        System.out.print("Enter a second string: ");
+        String stringTwo = scanner.nextLine();
+
+        return new TwoValues(stringOne, stringTwo);
+    }
+}
+
+record TwoValues(String first, String second) {
 }
