cb32281ed11f032c73af7c5eeffa42fa8640498e
Martin Blomberg <martin.blomberg@outlook.com>
2022-09-29 09:53:48 +0200

    Add animals

---
+++ src/se/iths/twentytwo/zoo/Animal.java
@@ -0,0 +1,49 @@
+package se.iths.twentytwo.zoo;
+
+import java.util.List;
+
+public class Animal {
+
+    public String getPetType() {
+        return "Normal animal";
+    }
+
+    public static void main(String[] args) {
+        Animal animal = new Animal();
+        Dog dog = new Dog();
+        Animal myDog = new Dog();
+        Animal myCat = new Cat();
+        takeToVet(myCat);
+        takeToVet(myDog, myCat);
+        takeToVet(myDog, myDog, myCat, myCat);
+    }
+
+    public static void takeToVet(Animal myPet) {
+        System.out.println("Taking my " + myPet.getPetType() + " to the vet");
+    }
+
+    public static void takeToVet(Animal myPet1, Animal myPet2) {
+        System.out.println("Taking my " + myPet1.getPetType() + " and " + myPet2.getPetType() + " to the vet");
+    }
+
+    public static void takeToVet(Animal... myPets) {
+        for (Animal animal : myPets) {
+            System.out.println("Taking my " + animal.getPetType() + " to the vet");
+        }
+    }
+
+}
+
+class Dog extends Animal {
+    @Override
+    public String getPetType() {
+        return "Dog";
+    }
+}
+
+class Cat extends Animal {
+    @Override
+    public String getPetType() {
+        return "Cat";
+    }
+}
