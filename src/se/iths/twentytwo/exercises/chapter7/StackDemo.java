88bc8154912992f6dfcb6e042cc2280c61f2dcc0
Martin Blomberg <martin.blomberg@outlook.com>
2022-09-30 13:06:00 +0200

    Add size method to Stack

--- src/se/iths/twentytwo/exercises/chapter7/StackDemo.java
+++ src/se/iths/twentytwo/exercises/chapter7/StackDemo.java
@@ -6,9 +6,11 @@ public class StackDemo {
         myStack.push("First string");
         myStack.push("Second string");
         myStack.push("Third string");
+        System.out.println(myStack.size()); //3
         System.out.println(myStack.pop());  //Third string
         System.out.println(myStack.pop());  //Second string
         System.out.println(myStack.pop());  //First string
-        System.out.println(myStack.pop());  //?
+        System.out.println(myStack.size()); //0
+        System.out.println(myStack.pop());  //Exception
     }
 }
