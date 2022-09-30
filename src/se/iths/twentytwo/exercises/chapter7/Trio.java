97b259de3720a587e1e7979c17a544ed3ab14156
Martin Blomberg <martin.blomberg@outlook.com>
2022-09-30 11:35:15 +0200

    Update Trio

--- src/se/iths/twentytwo/exercises/chapter7/Trio.java
+++ src/se/iths/twentytwo/exercises/chapter7/Trio.java
@@ -1,6 +1,12 @@
 package se.iths.twentytwo.exercises.chapter7;
 
-public class Trio<T1, T2, T3> {
+import se.iths.twentytwo.exercises.chapter6.vehicledealer.*;
+
+import se.iths.twentytwo.zoo.Animal;
+
+import java.math.BigDecimal;
+
+public class Trio<T1 extends LandBased, T2 extends Marine, T3 extends Airborne> {
 
     public T1 var1;
     public T2 var2;
@@ -10,5 +16,13 @@ public class Trio<T1, T2, T3> {
         this.var1 = var1;
         this.var2 = var2;
         this.var3 = var3;
+    }
+
+    public static void main(String[] args) {
+        Person stina = new Person("Stina", 35, "bankrånare", new BigDecimal(99999999));
+        Trio<Car, Boat, Helicopter> trioOfVehicles = new Trio<>(
+                new Car(2000, BigDecimal.valueOf(39999), stina),
+                new Boat(2000, BigDecimal.valueOf(39999), stina),
+                new Helicopter(2000, BigDecimal.valueOf(39999), stina));
     }
 }
