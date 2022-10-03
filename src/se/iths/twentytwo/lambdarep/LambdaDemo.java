812385fb8212fa3a66f66fcafac83382b53c201b
Martin Blomberg <martin.blomberg@outlook.com>
2022-10-03 10:34:30 +0200

    Add Runnable

---
+++ src/se/iths/twentytwo/lambdarep/LambdaDemo.java
@@ -0,0 +1,49 @@
+package se.iths.twentytwo.lambdarep;
+
+public class LambdaDemo {
+
+    public static void main(String[] args) {
+
+//        Calculatable calculatable = new Add();
+//        Calculatable calculatable = new Mul();
+//        Calculatable calculatable = new Calculatable() {
+//            @Override
+//            public int calculate(int a, int b) {
+//                System.out.println(this.getClass().getName());
+//                return a - b;
+//            }
+//        };
+        Calculatable calculatable = (a, b) -> a / b;
+
+        int result = calculatable.calculate(6, 3);
+        System.out.println(result);
+        System.out.println(calculatable.calculate(10, 2));
+        System.out.println(calculatable.calculate(5, 2));
+
+        Runnable runnable = () -> System.out.println("Hej");
+        runnable.run();
+        runnable.run();
+
+    }
+}
+
+
+@FunctionalInterface
+interface Calculatable {
+    int calculate(int a, int b);
+}
+
+class Add implements Calculatable {
+    @Override
+    public int calculate(int a, int b) {
+        return a + b;
+    }
+}
+
+class Mul implements Calculatable {
+
+    @Override
+    public int calculate(int a, int b) {
+        return a * b;
+    }
+}
