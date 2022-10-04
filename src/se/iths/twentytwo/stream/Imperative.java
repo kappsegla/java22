6af4135f067ed9c57a4f51b8b76a56fbc5b95517
Martin Blomberg <martin.blomberg@outlook.com>
2022-10-04 11:53:06 +0200

    Stream example

--- src/se/iths/twentytwo/stream/Imperative.java
+++ src/se/iths/twentytwo/stream/Imperative.java
@@ -1,35 +1,39 @@
 package se.iths.twentytwo.stream;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Stream;
 
 public class Imperative {
 
     public static void main(String[] args) {
 
-        List<String> stringList = List.of("A","B","C","D","E","F");
+        List<String> stringList = List.of("A", "B", "C", "D", "E", "F");
 
         boolean containsA = stringList.contains("A");
 
-        String[] arr = new String[]{"",""};
+        boolean answer = false;
+        for (int i = 0; i < stringList.size(); i++) {
+            if (stringList.get(i).equals("A")) {
+                answer = true;
+                break;
+            }
+        }
 
-        List<String> ls = new ArrayList<>(List.of(arr));
-        List<String> la = Arrays.asList(new String[5]);
-        List<String> lf = List.of(arr);
-        List<String> lc = List.copyOf(ls);
-        List<String> lu = Collections.unmodifiableList(ls);
+        stringList.forEach(s -> System.out.println(s));
 
-        handleList(ls);
+        for (String s : stringList) {
+            System.out.println(s);
+        }
+
+        Stream<String> li = Stream.empty();
+
+        stringList.stream()
+                .limit(4)
+                .skip(7)
+                .reduce((s, s2) -> String.valueOf((char) Math.max(s.charAt(0), s2.charAt(0))))
+                .ifPresent(System.out::println);
 
 
     }
-
-    public static void handleList(List<?> theList) {
-        if( !theList.isEmpty())
-            theList.remove(0);
-    }
-
-
 }
