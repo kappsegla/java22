290fbb66bdfd59e73f149df9fcb708f79b189042
Martin Blomberg <martin.blomberg@outlook.com>
2022-10-04 17:34:23 +0200

    Add more stream examples

--- src/se/iths/twentytwo/stream/StreamExamples.java
+++ src/se/iths/twentytwo/stream/StreamExamples.java
@@ -1,10 +1,14 @@
 package se.iths.twentytwo.stream;
 
+import java.util.Comparator;
 import java.util.List;
+
+import static java.util.function.Predicate.not;
 
 public class StreamExamples {
 
     public static void main(String[] args) {
+
         List<Integer> integerList = List.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
         List<String> stringList = List.of("Apple", "Bapelsin", "Citrus", "Tomato", "Kiwi", "Grape");
         List<Cat> catList = List.of(
@@ -15,23 +19,44 @@ public class StreamExamples {
                 new Cat("Luna", 2, true),
                 new Cat("Leo", 1, false));
 
-
         int sum = integerList.stream()
                 .mapToInt(i -> i)
-                .filter(StreamExamples::lessThanFive)
-                .sorted()
+                .filter(value -> value < 5)
                 .sum();
 
-        System.out.println(sum);
+        //System.out.println(sum);
 
-        long count  = integerList.stream()
-                .mapToInt(i->i)
-                .filter(StreamExamples::isOdd)
+        long count = integerList.stream()
+                .mapToInt(i -> i)
+                .filter(value -> isOdd(value))
                 .count();
-        System.out.println(count);
+        //System.out.println(count);
 
 
+        long catcount1 = catList.stream()
+                .filter(Cat::female)
+                .count();
+        System.out.println(catcount1);
 
+        long maleCount = catList.stream()
+                .filter(not(Cat::female))
+                .filter(cat -> cat.age() < 5)
+                .count();
+        System.out.println(catcount1);
+
+        double average = catList.stream()
+                .mapToInt(Cat::age)
+                .average()
+                .orElseThrow();
+        System.out.println(average);
+
+        //Sort according to number of characters in cat name
+        var result = catList.stream()
+                .sorted(Comparator.comparingInt(Cat::getNameLength)
+                        .thenComparing(Cat::name))
+                .toList();
+
+        result.forEach(System.out::println);
 
     }
 
@@ -44,26 +69,8 @@ public class StreamExamples {
     }
 }
 
-class Cat {
-    private final String name;
-    private final int age;
-    private final boolean female;
-
-    public Cat(String name, int age, boolean female) {
-        this.name = name;
-        this.age = age;
-        this.female = female;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public boolean isFemale() {
-        return female;
+record Cat(String name, int age, boolean female) {
+    public int getNameLength() {
+        return name.length();
     }
 }
