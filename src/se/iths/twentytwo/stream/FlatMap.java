bc32164cdb7ed1ff79240e4f2499c3cab3b47c66
Martin Blomberg <martin.blomberg@outlook.com>
2022-10-05 11:48:55 +0200

    Add Country and City flatMap example

--- src/se/iths/twentytwo/stream/FlatMap.java
+++ src/se/iths/twentytwo/stream/FlatMap.java
@@ -1,11 +1,18 @@
 package se.iths.twentytwo.stream;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class FlatMap {
 
     public static void main(String[] args) {
+        List<Country> countries = List.of(
+                new Country("Sweden",List.of(new City("Stockholm"),new City("Gothenburg"))),
+                new Country("Norway",List.of(new City("Oslo"),new City("Bergen"))));
 
+        countries.stream()
+                .flatMap(country -> country.cities().stream())
+                .forEach(System.out::println);
 
     }
 }
