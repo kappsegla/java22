277f672c323ad8e530ca390a17bad344b4e058d7
Martin Blomberg <martin.blomberg@outlook.com>
2022-10-05 13:03:58 +0200

    Remove unused imports

--- src/se/iths/twentytwo/stream/StreamsAndExceptions.java
+++ src/se/iths/twentytwo/stream/StreamsAndExceptions.java
@@ -3,7 +3,6 @@ package se.iths.twentytwo.stream;
 import java.util.List;
 import java.util.Optional;
 import java.util.function.Function;
-import java.util.stream.Collectors;
 
 import static java.util.stream.Collectors.*;
 
