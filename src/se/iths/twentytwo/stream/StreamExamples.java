394b7f8af6d4abe13af492a743956851353a8fb2
Martin Blomberg <martin.blomberg@outlook.com>
2022-10-05 09:34:58 +0200

    Add cat filter example

--- src/se/iths/twentytwo/stream/StreamExamples.java
+++ src/se/iths/twentytwo/stream/StreamExamples.java
@@ -2,13 +2,13 @@ package se.iths.twentytwo.stream;
 
 import java.util.Comparator;
 import java.util.List;
+import java.util.Scanner;
 
 import static java.util.function.Predicate.not;
 
 public class StreamExamples {
 
     public static void main(String[] args) {
-
         List<Integer> integerList = List.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
         List<String> stringList = List.of("Apple", "Bapelsin", "Citrus", "Tomato", "Kiwi", "Grape");
         List<Cat> catList = List.of(
@@ -17,47 +17,54 @@ public class StreamExamples {
                 new Cat("Snow white", 15, true),
                 new Cat("Bella", 5, true),
                 new Cat("Luna", 2, true),
-                new Cat("Leo", 1, false));
-
-        int sum = integerList.stream()
-                .mapToInt(i -> i)
-                .filter(value -> value < 5)
-                .sum();
-
-        //System.out.println(sum);
-
-        long count = integerList.stream()
-                .mapToInt(i -> i)
-                .filter(value -> isOdd(value))
-                .count();
-        //System.out.println(count);
-
-
-        long catcount1 = catList.stream()
-                .filter(Cat::female)
-                .count();
-        System.out.println(catcount1);
-
-        long maleCount = catList.stream()
-                .filter(not(Cat::female))
-                .filter(cat -> cat.age() < 5)
-                .count();
-        System.out.println(catcount1);
-
-        double average = catList.stream()
-                .mapToInt(Cat::age)
-                .average()
-                .orElseThrow();
-        System.out.println(average);
-
-        //Sort according to number of characters in cat name
-        var result = catList.stream()
-                .sorted(Comparator.comparingInt(Cat::getNameLength)
-                        .thenComparing(Cat::name))
-                .toList();
-
-        result.forEach(System.out::println);
-
+                new Cat("leo", 1, false),
+                new Cat("L", 0, false),
+                new Cat("", 0, false));
+//
+//        int sum = integerList.stream()
+//                .mapToInt(i -> i)
+//                .filter(value -> value < 5)
+//                .sum();
+//
+//        //System.out.println(sum);
+//
+//        long count = integerList.stream()
+//                .mapToInt(i -> i)
+//                .filter(value -> isOdd(value))
+//                .count();
+//        //System.out.println(count);
+//
+//
+//        long catcount1 = catList.stream()
+//                .filter(Cat::female)
+//                .count();
+//        System.out.println(catcount1);
+//
+//        long maleCount = catList.stream()
+//                .filter(not(Cat::female))
+//                .filter(cat -> cat.age() < 5)
+//                .count();
+//        System.out.println(catcount1);
+//
+//        double average = catList.stream()
+//                .mapToInt(Cat::age)
+//                .average()
+//                .orElseThrow();
+//        System.out.println(average);
+//
+//        //Sort according to number of characters in cat name
+//        var result = catList.stream()
+//                .sorted(Comparator.comparingInt(Cat::getNameLength)
+//                        .thenComparing(Cat::name))
+//                .toList();
+//        result.forEach(System.out::println);
+//        
+        catList.stream()
+                .map(Cat::name)
+                .map(String::toLowerCase)
+                .filter(name -> name.startsWith("l"))
+                .map(s -> s.substring(0, 1).toUpperCase() + s.substring(1))
+                .forEach(System.out::println);
     }
 
     private static boolean lessThanFive(int value) {
