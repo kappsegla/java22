94690c1002b739d54372c0dabbc75149a6e1df68
Martin Blomberg <martin.blomberg@outlook.com>
2022-10-10 10:32:02 +0200

    Generics Demo

--- src/se/iths/twentytwo/generics/GenericsDemo.java
+++ src/se/iths/twentytwo/generics/GenericsDemo.java
@@ -8,15 +8,36 @@ public class GenericsDemo {
         Storage<String> storage1 = new Storage<>("Hej");
         storage1.data = "Hej";
         var temp = storage1.getData();
-        
 
         ArrayList<String> list = new ArrayList<>();
         list.add("Hej");
         list.add("Hello");
+        ArrayList<Integer> list2 = new ArrayList<>();
+
+        printAll(list);
+        printAll(list2);
+
+        addStringToList(list);
 
         var secondItem = list.get(1);
         System.out.println(secondItem);
     }
+
+    public static void printAll(ArrayList<?> input) {
+
+    }
+
+    //pecs
+    //Producer extends consumer super
+    public static void addStringToList(ArrayList<? super String> input) {
+        input.add("Lunch time");
+    }
+
+    public static void readStringFromList(ArrayList<? extends String> input) {
+        var text = input.get(0);
+    }
+
+
 }
 
 class Storage<T> {
