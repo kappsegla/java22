b51a6cc9eddbd680d52785abb363f19491d278c7
Martin Blomberg <martin.blomberg@outlook.com>
2022-10-11 11:53:33 +0200

    Add write to file example

--- src/se/iths/twentytwo/files/FileExample.java
+++ src/se/iths/twentytwo/files/FileExample.java
@@ -1,5 +1,8 @@
 package se.iths.twentytwo.files;
 
+import java.io.IOException;
+import java.nio.file.*;
+
 public class FileExample {
 
     public static void main(String[] args) {
@@ -8,6 +11,27 @@ public class FileExample {
 
         String homeFolder = System.getProperty("user.home");
         System.out.println(homeFolder);
+
+        Path homePath = Path.of(homeFolder);
+        System.out.println(Files.exists(homePath));
+
+        Path filePath = Path.of(homeFolder, "test.txt");
+        System.out.println(filePath);
+
+        System.out.println(Files.exists(filePath));
+
+
+        try {
+            //Files.createFile(filePath);
+            //Files.move(filePath, Path.of(homeFolder,"moved.txt"));
+            Files.writeString(filePath, "Hello World\n");//, StandardOpenOption.APPEND);
+        }
+        catch( FileAlreadyExistsException e){
+            System.out.println("File already exists: " + e.getMessage());
+        }
+        catch (IOException e) {
+            System.out.println(e.getClass().getName() + " " + e.getMessage());
+        }
 
 
     }
