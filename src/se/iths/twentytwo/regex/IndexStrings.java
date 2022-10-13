4c374ec4e2e125492a672ffb27b0c6d9a1e742dc
Martin Blomberg <martin.blomberg@outlook.com>
2022-10-13 20:44:04 +0200

    Add bigram fuzzy compare

--- src/se/iths/twentytwo/regex/IndexStrings.java
+++ src/se/iths/twentytwo/regex/IndexStrings.java
@@ -2,10 +2,7 @@ package se.iths.twentytwo.regex;
 
 import se.iths.twentytwo.stream.Pair;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 import java.util.function.Function;
 import java.util.function.IntFunction;
 import java.util.regex.Pattern;
@@ -26,11 +23,13 @@ public class IndexStrings {
                 "My sister makes awesome fudge."
         );
 
-//        var indexMap = buildIndexMap(list);
-        var indexMap = buildIndexMapExcludingStopWords(list, Set.of("fudge", "sister"));
+        var indexMap = buildIndexMap(list);
+//        var indexMap = buildIndexMapExcludingStopWords(list, Set.of("fudge", "sister"));
 
         System.out.println(indexMap.entrySet());
         System.out.println("Documents containing fudge: " + indexMap.get("fudge"));
+
+        System.out.println(dice("fudge", "fudge"));
 
     }
 
@@ -68,5 +67,35 @@ public class IndexStrings {
 
     private static IntFunction<Pair<String, Integer>> createPairOfStringAndIndex(List<String> list, Pattern pattern) {
         return i -> Pair.of(pattern.matcher(list.get(i)).replaceAll("").toLowerCase(), i);
+    }
+
+    public static double dice(String string1, String string2) {
+        List<char[]> bigram1 = bigram(string1);
+        List<char[]> bigram2 = bigram(string2);
+        List<char[]> copy = new ArrayList<>(bigram2);
+        int matches = 0;
+        for (int i = bigram1.size(); --i >= 0; ) {
+            char[] bigram = bigram1.get(i);
+            for (int j = copy.size(); --j >= 0; ) {
+                char[] toMatch = copy.get(j);
+                if (bigram[0] == toMatch[0] && bigram[1] == toMatch[1]) {
+                    copy.remove(j);
+                    matches += 2;
+                    break;
+                }
+            }
+        }
+        return (double) matches / (bigram1.size() + bigram2.size());
+    }
+
+    private static List<char[]> bigram(String input) {
+        ArrayList<char[]> bigram = new ArrayList<char[]>();
+        for (int i = 0; i < input.length() - 1; i++) {
+            char[] chars = new char[2];
+            chars[0] = input.charAt(i);
+            chars[1] = input.charAt(i + 1);
+            bigram.add(chars);
+        }
+        return bigram;
     }
 }
