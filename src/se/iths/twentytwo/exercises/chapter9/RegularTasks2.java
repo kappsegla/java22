9f841889b6d6752a7f656ae9e8622313dc78cd49
Martin Blomberg <martin.blomberg@outlook.com>
2022-10-14 10:50:20 +0200

    Add regex task5

---
+++ src/se/iths/twentytwo/exercises/chapter9/RegularTasks2.java
@@ -0,0 +1,28 @@
+package se.iths.twentytwo.exercises.chapter9;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class RegularTasks2 {
+
+    public static final String TEXT = "hands jumbled terrific old-fashioned grass highfalutin stupendous daughter feigned skirt modern truthful";
+
+    public static void main(String[] args) {
+        Pattern pattern = Pattern.compile("as|od", Pattern.CASE_INSENSITIVE);
+
+        System.out.println(regexHit(pattern, TEXT));
+
+    }
+
+    private static int regexHit(Pattern pattern, String stringToBeTested) {
+        Matcher matcher = pattern.matcher(stringToBeTested);
+        return (int) matcher.results().count();
+//
+//        int count = 0;
+//        while (matcher.find()) {
+//            count++;
+//        }
+//        return count;
+
+    }
+}
