eb82e22f124c229d5470628008991f3acb0ea5ed
Martin Blomberg <martin.blomberg@outlook.com>
2022-10-28 10:45:28 +0200

    Add laboration2 Inventory

--- src/se/iths/twentytwo/laboration2/Inventory.java
+++ src/se/iths/twentytwo/laboration2/Inventory.java
@@ -1,9 +1,7 @@
 package se.iths.twentytwo.laboration2;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.math.BigDecimal;
+import java.util.*;
 
 public class Inventory {
 
@@ -11,9 +9,59 @@ public class Inventory {
 
     Map<Product, Integer> stock = new HashMap<>();
 
+    static final Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
         Inventory inventory = new Inventory();
+
+        inventory.createAndAddNewProduct(scanner);
+        Optional<Product> product = inventory.findProductByEanCode("1234567890123");
+        Optional<Product> productByName = inventory.findProductByName("Java coding");
+        List<Product> productsByPriceRange = inventory.findProductsByPriceRange(new BigDecimal("10.0"), new BigDecimal("100.0"));
+    }
+
+    public List<Product> findProductsByPriceRange(BigDecimal min, BigDecimal max) {
+        return productList.stream()
+                .filter(product -> product.getPrice().compareTo(min) > 0)
+                .filter(product -> product.getPrice().compareTo(max) < 0) //Med andra ord kan man skriva sub istället?
+                .toList();
+    }
+
+    public Optional<Product> findProductByName(String productName) {
+        return productList.stream()
+                .filter(product -> product.getProductName().equals(productName))
+                .findAny();
+    }
+
+    public Optional<Product> findProductByEanCode(String eanCode) {
+        return productList.stream()
+                .filter(product -> product.getEanCode().equals(eanCode))
+                .findAny();
+    }
+
+    public void createAndAddNewProduct(Scanner scanner) {
+
+        System.out.println("Skriv in produktnamn");
+        String productName = scanner.nextLine();
+        System.out.println("Skriv in ean code");
+        String eanCode = scanner.nextLine();
+        System.out.println("Skriv in produktens pris");
+        BigDecimal price = BigDecimal.valueOf(parseInput(scanner));
+        System.out.println("Skriv in kategori");
+        Category category = new Category(scanner.nextLine());
+
+        productList.add(new Product(productName, eanCode, price, category));
+        //Lite kod
+    }
+
+    private Double parseInput(Scanner sc) {
+        while (true) {
+            try {
+                return Double.parseDouble(sc.nextLine());
+            } catch (Exception e) {
+                System.out.println("Var vänlig skriv in ett tal.");
+            }
+        }
 
 
     }
